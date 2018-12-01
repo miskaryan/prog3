2e596ea26e5443174d5df929a810d9b4465e8c09
yourusername <your@email.com>
2018-12-01 15:27:48 +0400

    changes

---
+++ arj.js
@@ -0,0 +1,164 @@
+class Arj {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.energy = 15;
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1],
+
+            [this.x - 2, this.y - 2],
+            [this.x - 1, this.y - 2],
+            [this.x, this.y - 2],
+            [this.x + 1, this.y - 2],
+            [this.x + 2, this.y - 2],
+            [this.x - 2, this.y],
+            [this.x + 2, this.y],
+            [this.x - 2, this.y + 1],
+            [this.x - 2, this.y + 2],
+            [this.x - 1, this.y + 2],
+            [this.x + 1, this.y + 2],
+            [this.x, this.y + 2],
+            [this.x + 2, this.y + 2],
+            [this.x + 2, this.y + 1],
+            [this.x + 2, this.y - 1]
+
+
+        ]
+    }
+
+    getNewDirections() {
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1],
+
+            [this.x - 2, this.y - 2],
+            [this.x - 1, this.y - 2],
+            [this.x, this.y - 2],
+            [this.x + 1, this.y - 2],
+            [this.x + 2, this.y - 2],
+            [this.x - 2, this.y],
+            [this.x + 2, this.y],
+            [this.x - 2, this.y + 1],
+            [this.x - 2, this.y + 2],
+            [this.x - 1, this.y + 2],
+            [this.x + 1, this.y + 2],
+            [this.x, this.y + 2],
+            [this.x + 2, this.y + 2],
+            [this.x + 2, this.y + 1],
+            [this.x + 2, this.y - 1]
+
+
+        ]
+    }
+
+    chooseCell(character) {
+        this.getNewDirections()
+        var found = []
+        for (var i in this.directions) {
+            var x = this.directions[i][0]
+            var y = this.directions[i][1]
+            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+                if (matrix[y][x] == character) {
+                    found.push(this.directions[i])
+                }
+            }
+
+        }
+        return found;
+
+    }
+
+    mult() {
+        var empty = random(this.chooseCell(0))
+        if (empty && this.energy > 10) {
+            var newX = empty[0]
+            var newY = empty[1]
+            matrix[newY][newX] = 4
+            var ar = new Arj(newX, newY)
+            arjArr.push(ar)
+        }
+    }
+
+    move() {
+        var empty = random(this.chooseCell(0))
+        this.energy -=2;
+        if (empty) {
+            var newX = empty[0]
+            var newY = empty[1]
+            matrix[newY][newX] = 4
+            matrix[this.y][this.x] = 1
+
+            var gr = new Grass(this.x, this.y)
+            grassArr.push(gr)
+
+
+            this.x = newX
+            this.y = newY
+        }
+    }
+
+    eat() {
+        var food = random(this.chooseCell(3))
+
+        if (food) {
+            var newX = food[0]
+            var newY = food[1]
+            matrix[newY][newX] = 4
+            matrix[this.y][this.x] = 0
+
+            for (var i in gishatichArr) {
+                if (gishatichArr[i].x == newX && gishatichArr[i].y == newY) {
+                    gishatichArr.splice(i, 1)
+                }
+            }
+
+            this.x = newX
+            this.y = newY
+            this.energy += 3
+        }
+    }
+    eat1() {
+        var food = random(this.chooseCell(5))
+
+        if (food) {
+            var newX = food[0]
+            var newY = food[1]
+            matrix[newY][newX] = 4
+            matrix[this.y][this.x] = 0
+
+            for (var i in ocArr) {
+                if (ocArr[i].x == newX && ocArr[i].y == newY) {
+                    ocArr.splice(i, 1)
+                }
+            }
+
+            this.x = newX
+            this.y = newY
+            this.energy += 3
+        }
+    }
+
+    die() {
+        if (this.energy <= 0) {
+            matrix[this.y][this.x] = 0
+            for (var i in arjArr) {
+                if (arjArr[i].x == this.x && arjArr[i].y == this.y) {
+                    arjArr.splice(i, 1)
+                }
+            }
+        }
+    }
+}
---
+++ oc.js
@@ -0,0 +1,154 @@
+class Oc {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.energy = 15;
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1],
+
+            [this.x - 2, this.y - 2],
+            [this.x - 1, this.y - 2],
+            [this.x, this.y - 2],
+            [this.x + 1, this.y - 2],
+            [this.x + 2, this.y - 2],
+            [this.x - 2, this.y],
+            [this.x + 2, this.y],
+            [this.x - 2, this.y + 1],
+            [this.x - 2, this.y + 2],
+            [this.x - 1, this.y + 2],
+            [this.x + 1, this.y + 2],
+            [this.x, this.y + 2],
+            [this.x + 2, this.y + 2],
+            [this.x + 2, this.y + 1],
+            [this.x + 2, this.y - 1]
+        ]
+    }
+
+    getNewDirections() {
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1],
+
+            [this.x - 2, this.y - 2],
+            [this.x - 1, this.y - 2],
+            [this.x, this.y - 2],
+            [this.x + 1, this.y - 2],
+            [this.x + 2, this.y - 2],
+            [this.x - 2, this.y],
+            [this.x + 2, this.y],
+            [this.x - 2, this.y + 1],
+            [this.x - 2, this.y + 2],
+            [this.x - 1, this.y + 2],
+            [this.x + 1, this.y + 2],
+            [this.x, this.y + 2],
+            [this.x + 2, this.y + 2],
+            [this.x + 2, this.y + 1],
+            [this.x + 2, this.y - 1],
+        ]
+    }
+
+    chooseCell(character) {
+        this.getNewDirections()
+        var found = []
+        for (var i in this.directions) {
+            var x = this.directions[i][0]
+            var y = this.directions[i][1]
+            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+                if (matrix[y][x] == character) {
+                    found.push(this.directions[i])
+                }
+            }
+
+        }
+        return found;
+
+    }
+
+    mult() {
+        var empty = random(this.chooseCell(0))
+        if (empty && this.energy > 10) {
+            var newX = empty[0]
+            var newY = empty[1]
+            matrix[newY][newX] = 5
+            var o = new Oc(newX, newY)
+            ocArr.push(o)
+        }
+    }
+
+    move() {
+        var empty = random(this.chooseCell(0))
+        this.energy-=2;
+        if (empty) {
+            var newX = empty[0]
+            var newY = empty[1]
+            matrix[newY][newX] = 5
+            matrix[this.y][this.x] = 0
+
+            this.x = newX
+            this.y = newY
+        }
+    }
+
+    eat() {
+        var food = random(this.chooseCell(2))
+        if (food) {
+            var newX = food[0]
+            var newY = food[1]
+            matrix[newY][newX] = 5
+            matrix[this.y][this.x] = 0
+
+            for (var i in xotakerArr) {
+                if (xotakerArr[i].x == newX && xotakerArr[i].y == newY) {
+                    xotakerArr.splice(i, 1)
+                }
+            }
+
+            this.x = newX
+            this.y = newY
+            this.energy += 3
+        }
+    }
+    eat1() {
+        var food = random(this.chooseCell(1))
+        if (food) {
+            var newX = food[0]
+            var newY = food[1]
+            matrix[newY][newX] = 5
+            matrix[this.y][this.x] = 0
+
+            for (var i in grassArr) {
+                if (grassArr[i].x == newX && grassArr[i].y == newY) {
+                    grassArr.splice(i, 1)
+                }
+            }
+
+            this.x = newX
+            this.y = newY
+            this.energy += 3
+        }
+    }
+
+    die() {
+        if (this.energy <= 0) {
+            matrix[this.y][this.x] = 0
+            for (var i in ocArr) {
+                if (ocArr[i].x == this.x && ocArr[i].y == this.y) {
+                    ocArr.splice(i, 1)
+                }
+            }
+        }
+    }
+}
